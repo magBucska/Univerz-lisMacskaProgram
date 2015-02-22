2fe7d7675eb33d9fd1a097ed5b0f23d6b1db0c89
bucska <bucska.mag@gmail.com>
2015-02-22 11:44:25 +0100

    wdasdefedesdsg

--- src/mag/bucska/Macska.java
+++ src/mag/bucska/Macska.java
@@ -2,23 +2,40 @@ package mag.bucska;
 
 import java.util.HashMap;
 import java.util.Map;
-
+                                                 
 public class Macska {
 	Map<String, String> macskaMap ;
 	public String nev; 
 	
 	public Macska (String neve) {
-		this.nev = neve;
+		nev = neve;
 		macskaMap = new HashMap<String, String>();
-		macskaMap.put("Hal", "Béla Ugrik");
-		macskaMap.put("Májkrém", "Béla Dorombol"); 
-		macskaMap.put("Csont", "Béla Nyavog");
-		macskaMap.put("Egér", "Béla Üvölt");
-		macskaMap.put ("Nátha", "Béla Szipog");
+		macskaMap.put("Hal", "Ugrik");
+		macskaMap.put("Májkrém", "Dorombol"); 
+		macskaMap.put("Csont", "Nyavog");
+		macskaMap.put("Egér", "Üvölt");
+		macskaMap.put ("Nátha", "Szipog");
 	}
 
 	public String kap(String kaja) {
-		return macskaMap.get(kaja);
+		String muvelet = macskaMap.get(kaja);
+		return nev + " " + muvelet;
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
